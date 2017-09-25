6792bf8c347bc44cef91c0346bbbf25ebc3ac8dd
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro.local>
2017-09-25 16:57:32 +0700

    as

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -7,9 +7,7 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  $.ajaxSetup({
-     headers: {'X-CSRF-Token': $('meta[name=csrf_token]').attr('content')}
-  });
+  
   <base href="{{asset('')}}">
 
   <link rel="stylesheet" href="{{ url('admin/css/bootstrap.min.css') }}">
