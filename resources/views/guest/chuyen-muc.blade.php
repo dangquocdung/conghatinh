247d1e41b0cd0d2b5fb7744428069c873b1c90a8
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-11-01 23:46:52 +0700

    ss

--- resources/views/guest/chuyen-muc.blade.php
+++ resources/views/guest/chuyen-muc.blade.php
@@ -26,8 +26,18 @@
                               <span class="label label-info pull-right">{{$tin->loaitin->name}}</span>
 
                               <a class="tin_title_text" href="/chi-tiet/{{$tin->slug}}">
-                                
-                                  <img style="display: inline-block; width: 160px; height:auto;" src="{{$tin->avatar}}" alt="" title="">
+
+                                  @if ($cm->slug <> 'doanh-nghiep-du-an')
+
+                                      <img src="{{$tin->avatar}}" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 160px; height:auto;" >
+
+                                  @else
+
+                                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWpS3UrDgKd7jcT3BkbPkU4d0mzV7c6PRQ5JmNQIv2Mu2eQ_UpMA" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 80px; height:auto;" >
+
+
+                                  @endif
+
                                   <div class="tin_title_text">
                                       {{$tin->name}}
                                   </div>
@@ -38,6 +48,14 @@
 
                                   {{ $tin->gioithieu}}
                               </div>
+
+                                <div class="pull-right" style="padding-top: 7px;">
+                                    @foreach($tin->teptintuc as $ttt)
+                                        <a href="{{ $ttt->path }}" target="_blank">
+                                            <i class="fa fa-file-pdf-o fa-2x" aria-hidden="true" style="color:red"></i>
+                                        </a>
+                                    @endforeach
+                                </div>
                           </div>
                         </div>
                        @endforeach
