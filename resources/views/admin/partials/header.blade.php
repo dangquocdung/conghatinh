99c048340de658966e1dac9bf36b7a312c8e3104
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-11-06 10:25:15 +0700

    ss

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -11,6 +11,7 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="icon" href="./images/favicon.ico">
 
 
 
