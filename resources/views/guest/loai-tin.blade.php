3e07df887bb9d23e90e40ce4c1a04be50f3d9a8f
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-11-07 11:52:09 +0700

    ss

--- resources/views/guest/loai-tin.blade.php
+++ resources/views/guest/loai-tin.blade.php
@@ -24,6 +24,7 @@
 
                             <div class="news-main" style="padding: 0;">
                                 <div class="row" style="padding: 0 15px 10px 15px; border-bottom: 1px solid #eaeaea;">
+
                                   <span class="label label-info pull-right hidden-xs">{{$tin->loaitin->name}}</span>
                                   <a class="tin_title_text" href="/chi-tiet/{{$tin->slug}}">
 
@@ -59,8 +60,9 @@
                                         @endforeach
                                     </div>
                                 </div>
+
                           </div>
-                        </div>
+                        
                        @endforeach
                     </div>
 
