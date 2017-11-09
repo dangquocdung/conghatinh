a8847c8a015a70828b4598e7c1aa36517bf06cf5
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-11-09 16:42:22 +0700

    ss

--- resources/views/guest/home/van-ban.blade.php
+++ resources/views/guest/home/van-ban.blade.php
@@ -18,49 +18,40 @@
 
         @php
 
-          $tins = $lt->vanban->sortByDesc('id')->take(5);
+          $tins = $lt->vanban->sortByDesc('id')->take(4);
 
 
         @endphp
 
 
-
-          
-
-    
-         
-
-            @if ($tins)
-            <div class="col-md-6" style="float:left">
+        @if ($tins)
+            <div class="col-md-12" style="float:left">
                 <div class="row">
 
-            <div class="breadcrumb" style="margin-left: 3px; margin-right: 3px">
-                <span class="breadcrumb-item active">
-                  <a href="van-ban/{{ $lt->slug }}" style="text-decoration: none;">
-                    <span class="glyphicon glyphicon-share-alt"></span> 
-                    <strong>{{ $lt->name }}</strong>
-                  </a>
-                </span>
-                <span class="pull-right">
-                    <a href="van-ban/{{ $lt->slug }}" style="text-decoration: none;"><em><small><i class="fa fa-angle-double-right" aria-hidden="true"></i>Xem tiếp...</small></em></a>
-                </span>
-            </div>
-
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="row">
-
+                    <div class="breadcrumb" style="margin-left: 3px; margin-right: 3px">
+                        <span class="breadcrumb-item active">
+                          <a href="van-ban/{{ $lt->slug }}" style="text-decoration: none;">
+                            <span class="glyphicon glyphicon-share-alt"></span>
+                            <strong>{{ $lt->name }}</strong>
+                          </a>
+                        </span>
+                        <span class="pull-right">
+                            <a href="van-ban/{{ $lt->slug }}" style="text-decoration: none;"><em><small><i class="fa fa-angle-double-right" aria-hidden="true"></i>Xem tiếp...</small></em></a>
+                        </span>
+                    </div>
                     <div class="news-vanban">
                         <ul class="news-block">
                             @foreach($tins as $vb)
-                                <li>
 
-                                    
+                                <li style="margin-top: 0; border-bottom: none;">
+
+
 
                                         <a href="chi-tiet-van-ban/{{$vb->id}}" class="news-title bold">
                                             <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
-                                            Số: {{ $vb->kihieuvb }}
+                                            Số: {{ $vb->kihieuvb }} <span class="label label-danger">Mới</span>
                                         </a>
-                                        
+
 
                                         @foreach($vb->tepvanban as $tvb)
 
@@ -75,24 +66,14 @@
                                             {{$vb->trichyeu}}
                                         </div>
 
-                                    
+
                                 </li>
                             @endforeach
-
-                            {{-- <li style="text-align: right;">
-
-                              <a href="van-ban/{{ $lt->slug }}" style="text-decoration: none;"><em>Xem tiếp... <i class="fa fa-angle-double-right" aria-hidden="true"></i></em></a>
-                          
-                          
-                            </li> --}}
-                        </ul>
-
+                         </ul>
                     </div>
                 </div>
             </div>
-            </div>
-            </div>
-            @endif
+        @endif
 
     
     @endforeach
