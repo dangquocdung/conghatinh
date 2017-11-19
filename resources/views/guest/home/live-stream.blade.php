1ec0d96d872ca4b679315ccbe9c7af20056dea7f
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-11-20 00:00:30 +0700

    ss

--- resources/views/guest/home/live-stream.blade.php
+++ resources/views/guest/home/live-stream.blade.php
@@ -3,8 +3,8 @@
     <div class="block4" id="live-stream">
 
         <div class="embed-responsive embed-responsive-16by9">
-{{--            {{Setting::get('video_livestream')}}--}}
-            <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fhttvonline%2Fvideos%2F363374970780871%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+            {{Setting::get('video_livestream')}}
+            {{--<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fhttvonline%2Fvideos%2F363374970780871%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>--}}
 
         </div>
 
