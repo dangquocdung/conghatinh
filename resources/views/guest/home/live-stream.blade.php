d04ad29c6fb83311bb738739e9a1c7811d5809a2
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-11-25 19:55:48 +0700

    ss

--- resources/views/guest/home/live-stream.blade.php
+++ resources/views/guest/home/live-stream.blade.php
@@ -1,10 +1,10 @@
-@if (strlen(Setting::get('video_livestream')) > 100 )
+@if (strlen(Setting::get('video_livestream')) > 10 )
 
     <div class="block4" id="live-stream" style="border-radius: 5px">
 
         <div class="embed-responsive embed-responsive-16by9">
 
-            <iframe src="{!! Setting::get('video_livestream') !!}" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+            <iframe src="{{Setting::get('video_livestream')}}" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
 
         </div>
 
