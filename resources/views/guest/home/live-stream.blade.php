30116c8d3d4a3d8aa869af5ad5f701953bab21a4
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-11-25 20:02:22 +0700

    ss

--- resources/views/guest/home/live-stream.blade.php
+++ resources/views/guest/home/live-stream.blade.php
@@ -4,7 +4,7 @@
 
         <div class="embed-responsive embed-responsive-16by9">
 
-            <iframe class="embed-responsive-item" src="{!! Setting::get('video_livestream') !!}"></iframe>
+            <iframe class="embed-responsive-item" src="http://hatinhtv.vn/videos/thoisu/19/TS_ThoiSuToi_241117.mp4"></iframe>
 
         </div>
 
