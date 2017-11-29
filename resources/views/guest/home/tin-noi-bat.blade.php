158ba3fbca1942e19c2e0ca7a226789e837116ae
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-11-29 14:52:36 +0700

    ss

--- resources/views/guest/home/tin-noi-bat.blade.php
+++ resources/views/guest/home/tin-noi-bat.blade.php
@@ -77,20 +77,20 @@
     @endif
 </div>
 
-<div class="block2">
 
 
-    @foreach ($banner as $bn)
-        @if ($bn->vitri == 6)
-            <div class="col-md-3 col-sm-3 col-xs-6 box">
-                <a href="{{$bn->lienket}}" target="_blank">
-                    <img src="{{ $bn->banner}}" alt="{{ $bn->name}}" title="{{ $bn->name}}" width="100%">
-                </a>
-            </div>
-        @endif
-    @endforeach
 
-</div>
+@foreach ($banner as $bn)
+    @if ($bn->vitri == 6)
+        <div class="col-md-3 col-sm-3 col-xs-6 box">
+            <a href="{{$bn->lienket}}" target="_blank">
+                <img src="{{ $bn->banner}}" alt="{{ $bn->name}}" title="{{ $bn->name}}" width="100%">
+            </a>
+        </div>
+    @endif
+@endforeach
+
+
 
 
 <div class="block2">
