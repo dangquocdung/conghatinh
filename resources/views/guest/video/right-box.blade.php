2f9255bfe5eaa5de0e9c325b72382a8f95d6914e
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-07 14:04:56 +0700

    ss

--- resources/views/guest/video/right-box.blade.php
+++ resources/views/guest/video/right-box.blade.php
@@ -1,27 +1,4 @@
 
-        <div class="block4">
-            <div class="block-header">
-                <a href="">
-                    <h4><img src="/images/background/lotus.ico" alt="" width="26px"> Video nổi bật</h4>
-                </a>
-            </div>
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                @foreach($vd_noibat->sortByDesc('ngayphat') as $vd)
-                    <div class="video-thumbnail">
-                        <a href="{{ route('thu-vien-video',$vd->id) }}}">
-                            <img data-u="image" src="{{ $vd->loaivideo->cover_image }}" title="{{ $vd->name }}" width="100%" />
-                        </a>
-
-                    </div>
-
-                    <div class="tieu-de" style="text-align: center; margin-top: 5px">
-                        <a href="{{ route('thu-vien-video',$vd->id) }}">
-                            {{ $vd->loaivideo->name }} ngày {{ \Carbon\Carbon::parse($vd->ngayphat)->format('d-m-Y') }}
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-        </div>
 
 
 
