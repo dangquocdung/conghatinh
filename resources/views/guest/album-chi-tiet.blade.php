189c140a5f8497a33c9dbb8b04f59970baa6f7e8
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-11 23:03:32 +0700

    ss

--- resources/views/guest/album-chi-tiet.blade.php
+++ resources/views/guest/album-chi-tiet.blade.php
@@ -44,6 +44,31 @@
 
             });
         </script>
+
+        <br>
+
+        <div class="list-image-view">
+            <ul style="padding: 0;">
+                @foreach($albums as $ab)
+                    <li class="col-md-3 col-sm-3 col-xs-6">
+                        <div class="album-item">
+                            <a href="{{ Request::path().'/'.$ab->slug }}">
+                                <img src="{{ $ab->cover_image }}" alt="{{ $ab->name }}" title="{{ $ab->name }}" style="height: 100%">
+                                <h5>{{ $ab->name }}</h5>
+                            </a>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+
+        <script>
+            $(document).ready(function() {
+                /* This is basic - uses default settings */
+                $("a.single_image").fancybox();
+            });
+        </script>
+
     </div>
 
 @endsection
