171424f49384499cba1defcab5d092a8009aeaed
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-11 09:48:44 +0700

    ss

--- resources/views/guest/menu-right/tim-kiem.blade.php
+++ resources/views/guest/menu-right/tim-kiem.blade.php
@@ -29,7 +29,7 @@
 
         <div class="input-group-btn search-panel">
             <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                <span id="search_concept">Tiêu đề</span> <span class="caret"></span>
+                <span id="search_concept">Tin tức</span> <span class="caret"></span>
             </button>
             <ul class="dropdown-menu" role="menu">
                 <li><a href="#gioithieu">Giới thiệu</a></li>
