7457d4aeaaf20bbb0705199627a39edc7b6a19bd
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-12 09:02:58 +0700

    ss

--- resources/views/guest/album-chi-tiet.blade.php
+++ resources/views/guest/album-chi-tiet.blade.php
@@ -7,7 +7,7 @@
 
     <div class="block3">
         <div class="portlet-header">
-            <a href="/loai-tin/album-hinh-anh">
+            <a href=" {{ route('album-hinh-anh') }}">
                 <h4 class="portlet-header-title no-pd-top"><img src="/images/background/lotus.ico" alt="" width="26px"> Tất cả Album hình ảnh</h4>
             </a>
         </div>
