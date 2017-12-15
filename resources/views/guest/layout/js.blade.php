21527a6ae8d6bcf9057617bc52719d0af5f29c8c
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-15 12:07:14 +0700

    ss

--- resources/views/guest/layout/js.blade.php
+++ resources/views/guest/layout/js.blade.php
@@ -48,9 +48,9 @@
 
 
 <script>
-    jssor_1_slider_init = function() {
-
-        var jssor_1_SlideshowTransitions = [
+    jssor_slider1_init = function (containerId) {
+        //Define an array of slideshow transition code
+        var _SlideshowTransitions = [
             {$Duration:500,$Delay:12,$Cols:10,$Rows:5,$Opacity:2,$Clip:15,$SlideOut:true,$Formation:$JssorSlideshowFormations$.$FormationStraightStairs,$Assembly:2049,$Easing:$Jease$.$OutQuad},
             {$Duration:500,$Delay:40,$Cols:10,$Rows:5,$Opacity:2,$Clip:15,$SlideOut:true,$Easing:$Jease$.$OutQuad},
             {$Duration:1000,x:-0.2,$Delay:20,$Cols:16,$SlideOut:true,$Formation:$JssorSlideshowFormations$.$FormationStraight,$Assembly:260,$Easing:{$Left:$Jease$.$InOutExpo,$Opacity:$Jease$.$InOutQuad},$Opacity:2,$Outside:true,$Round:{$Top:0.5}},
@@ -60,38 +60,29 @@
             {$Duration:1500,x:0.3,y:-0.3,$Delay:20,$Cols:10,$Rows:5,$Opacity:2,$Clip:15,$During:{$Left:[0.1,0.9],$Top:[0.1,0.9]},$SlideOut:true,$Formation:$JssorSlideshowFormations$.$FormationStraightStairs,$Assembly:260,$Easing:{$Left:$Jease$.$InJump,$Top:$Jease$.$InJump,$Clip:$Jease$.$OutQuad},$Round:{$Left:0.8,$Top:2.5}}
         ];
 
-        var jssor_1_options = {
+
+        var options = {
             $AutoPlay: 1,
-            $Cols: 1,
-            $Align: 0,
             $SlideshowOptions: {
                 $Class: $JssorSlideshowRunner$,
-                $Transitions: jssor_1_SlideshowTransitions,
-                $TransitionsOrder: 1
-            },
-            $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$
-            },
-            $BulletNavigatorOptions: {
-                $Class: $JssorBulletNavigator$
+                $Transitions: _SlideshowTransitions,
+                $TransitionsOrder: 1,
+                $ShowLink: true
             }
         };
-
-        var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-
-        /*#region responsive code begin*/
+        var jssor_slider1 = new $JssorSlider$(containerId, options);
 
         var MAX_WIDTH = 1200;
 
         function ScaleSlider() {
-            var containerElement = jssor_1_slider.$Elmt.parentNode;
+            var containerElement = jssor_slider1.$Elmt.parentNode;
             var containerWidth = containerElement.clientWidth;
 
             if (containerWidth) {
 
                 var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
 
-                jssor_1_slider.$ScaleWidth(expectedWidth);
+                jssor_slider1.$ScaleWidth(expectedWidth);
             }
             else {
                 window.setTimeout(ScaleSlider, 30);
@@ -100,10 +91,9 @@
 
         ScaleSlider();
 
-        $Jssor$.$AddEvent(window, "load", ScaleSlider);
-        $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-        $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-        /*#endregion responsive code end*/
+        $(window).bind("load", ScaleSlider);
+        $(window).bind("resize", ScaleSlider);
+        $(window).bind("orientationchange", ScaleSlider);
     };
 
 </script>
