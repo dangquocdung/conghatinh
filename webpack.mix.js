2b6c1ee2a02e023023e249c52fad166b8c36b9e5
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-15 10:08:12 +0700

    ss

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.js('resources/assets/js/app.js', 'public/admin/js')
     .sass('resources/assets/sass/admin.scss', 'public/admin/css')
 	.js('resources/assets/js/guest.js', 'public/js')
     .sass('resources/assets/sass/guest.scss', 'public/guest/css')
