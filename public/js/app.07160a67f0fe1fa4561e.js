a58373317731367f25e0f6d5e54ee265d8b0bc26
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-15 09:53:07 +0700

    ss

--- public/js/app.07160a67f0fe1fa4561e.js
+++ public/js/app.07160a67f0fe1fa4561e.js
@@ -18907,7 +18907,7 @@ exports.push([module.i, "\n.Image-upload .Modal {\n  border-top: 1px solid #f4f4
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__("./node_modules/css-loader/lib/css-base.js")();
-exports.push([module.i, "\n.galleryWrapper ul {\n  padding: 0;\n  margin: 0;\n}\n.galleryWrapper ul li {\n    list-style: none;\n    float: left;\n    padding: 8px;\n    margin: 0;\n}\n.galleryWrapper ul li .thumbnail {\n      position: relative;\n      width: 70px !important;\n      height: 60px !important;\n      overflow: hidden;\n}\n.galleryWrapper ul li .thumbnail img {\n        position: absolute;\n        left: 50%;\n        top: 50%;\n        height: 100%;\n        width: auto;\n        -webkit-transform: translate(-50%, -50%);\n        transform: translate(-50%, -50%);\n}\n.media-manager-details .modal-content {\n  width: 80%;\n}\n.media-manager-details .modal-content .big-image img {\n    max-width: 660px;\n}\n", ""]);
+exports.push([module.i, "\n.galleryWrapper ul {\n  padding: 0;\n  margin: 0;\n}\n.galleryWrapper ul li {\n    list-style: none;\n    float: left;\n    padding: 8px;\n    margin: 0;\n}\n.galleryWrapper ul li .thumbnail {\n      position: relative;\n      width: 70px !important;\n      height: 60px !important;\n      overflow: hidden;\n}\n.galleryWrapper ul li .thumbnail img {\n        position: absolute;\n        left: 50%;\n        top: 50%;\n        height: 100%;\n        width: 100%;\n        -webkit-transform: translate(-50%, -50%);\n        transform: translate(-50%, -50%);\n}\n.media-manager-details .modal-content {\n  width: 80%;\n}\n.media-manager-details .modal-content .big-image img {\n    max-width: 660px;\n}\n", ""]);
 
 /***/ }),
 
