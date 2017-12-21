7d8306a3519f87541bcb0d386c4e9b5f68a5159a
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-21 10:57:41 +0700

    ss

--- resources/views/guest/home/chi-dao-dieu-hanh.blade.php
+++ resources/views/guest/home/chi-dao-dieu-hanh.blade.php
@@ -3,10 +3,11 @@
         <div class="block3">
             <div class="box box-default">
                 <div class="box-header with-border">
-                    <img src="/images/background/lotus.ico" width="30px">
+                    <img src="/images/background/lotus.ico" width="30px" style="padding: 3px">
                     <h4 class="box-title">{{ $cm->name }}</h4>
 
                     <div class="box-tools pull-right">
+                        <a class="btn btn-box-tool" href="{{ route('chuyen-muc',$cm->slug) }}"><i class="fa  fa-folder-open"></i></a>
                         <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                         <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
                     </div>
