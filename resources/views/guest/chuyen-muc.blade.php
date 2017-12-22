7d73b0af34cea5a2a6f294d18d64908867b4cf9c
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-22 14:53:05 +0700

    ss

--- resources/views/guest/chuyen-muc.blade.php
+++ resources/views/guest/chuyen-muc.blade.php
@@ -17,67 +17,67 @@
         <div class="loai-tin">
             @foreach ($cm->loaitin as $lt)
 
-                <div class="lienquan-header" style="margin-bottom: 10px">
+                @if (count($lt->tintuc) > 0)
+                    <div class="lienquan-header" style="margin-bottom: 10px">
+                        <a href="{{ route('loai-tin',[$cm->slug, $lt->slug])  }}">{{ $lt->name }}</a>
+                    </div>
 
-                    <a href="{{ route('loai-tin',[$cm->slug, $lt->slug])  }}">{{ $lt->name }}</a>
+                    <br>
 
-                </div>
+                    @foreach ($lt->tintuc->where('daduyet','1')->sortbydesc('id')->take(5) as $tin)
 
-                <br>
+                        <div class="news-main" style="padding: 0">
+                            <div class="row" style="padding: 0 15px 10px 15px; border-bottom: 1px solid #eaeaea;">
 
-                @foreach ($lt->tintuc->where('daduyet','1')->sortbydesc('id')->take(5) as $tin)
+                                  <span class="label label-info pull-right hidden-xs" style="margin-left: 15px;">{{$tin->loaitin->name}}</span>
 
-                    <div class="news-main" style="padding: 0">
-                        <div class="row" style="padding: 0 15px 10px 15px; border-bottom: 1px solid #eaeaea;">
+                                  <a class="tin_title_text" href="{{route('chi-tiet-tin',[$tin->loaitin->chuyenmuc->slug,$tin->loaitin->slug,$tin->slug])}}">
 
-                              <span class="label label-info pull-right hidden-xs" style="margin-left: 15px;">{{$tin->loaitin->name}}</span>
+                                      {{--@if ($cm->slug <> 'doanh-nghiep-du-an')--}}
 
-                              <a class="tin_title_text" href="{{route('chi-tiet-tin',[$tin->loaitin->chuyenmuc->slug,$tin->loaitin->slug,$tin->slug])}}">
+                                          {{--<img src="{{$tin->avatar}}" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 160px; height:auto;" >--}}
 
-                                  {{--@if ($cm->slug <> 'doanh-nghiep-du-an')--}}
+                                      {{--@else--}}
 
-                                      {{--<img src="{{$tin->avatar}}" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 160px; height:auto;" >--}}
-
-                                  {{--@else--}}
-
-                                      {{--<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWpS3UrDgKd7jcT3BkbPkU4d0mzV7c6PRQ5JmNQIv2Mu2eQ_UpMA" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 80px; height:auto;" >--}}
+                                          {{--<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWpS3UrDgKd7jcT3BkbPkU4d0mzV7c6PRQ5JmNQIv2Mu2eQ_UpMA" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 80px; height:auto;" >--}}
 
 
-                                  {{--@endif--}}
+                                      {{--@endif--}}
 
-                                  @if (strlen(trim($tin->avatar)) > 20)
-                                      <img src="{{$tin->avatar}}" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 160px; height:auto;" >
-                                  @else
-                                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWpS3UrDgKd7jcT3BkbPkU4d0mzV7c6PRQ5JmNQIv2Mu2eQ_UpMA" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 80px; height:auto;" >
-                                  @endif
+                                      @if (strlen(trim($tin->avatar)) > 20)
+                                          <img src="{{$tin->avatar}}" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 160px; height:auto;" >
+                                      @else
+                                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWpS3UrDgKd7jcT3BkbPkU4d0mzV7c6PRQ5JmNQIv2Mu2eQ_UpMA" alt="{{$tin->name}}" title="{{$tin->name}}" style="display: inline-block; width: 80px; height:auto;" >
+                                      @endif
 
-                                  <div class="tin_title_text">
-                                      {{$tin->name}} <small><em style="font-weight: normal">({{ \Carbon\Carbon::parse($tin->ngaydang)->format('d-m-Y H:i:s')}})</em></small>
+                                      <div class="tin_title_text">
+                                          {{$tin->name}} <small><em style="font-weight: normal">({{ \Carbon\Carbon::parse($tin->ngaydang)->format('d-m-Y H:i:s')}})</em></small>
+                                      </div>
+
+                                  </a>
+
+                                  <div class="tin_title_abstract" style="display:;">
+
+                                      {{ $tin->gioithieu}}
                                   </div>
 
-                              </a>
-
-                              <div class="tin_title_abstract" style="display:;">
-
-                                  {{ $tin->gioithieu}}
-                              </div>
-
-                            <div class="pull-right" style="padding-top: 7px;">
-                                @foreach($tin->teptintuc as $ttt)
-                                    <a href="{{ $ttt->path }}" target="_blank">
-                                        <i class="fa fa-file-pdf-o fa-2x" aria-hidden="true" style="color:red"></i>
-                                    </a>
-                                @endforeach
+                                <div class="pull-right" style="padding-top: 7px;">
+                                    @foreach($tin->teptintuc as $ttt)
+                                        <a href="{{ $ttt->path }}" target="_blank">
+                                            <i class="fa fa-file-pdf-o fa-2x" aria-hidden="true" style="color:red"></i>
+                                        </a>
+                                    @endforeach
+                                </div>
                             </div>
-                        </div>
 
 
 
-                  </div>
+                      </div>
 
 
-                @endforeach
-                <div class="clearfix"></div>
+                    @endforeach
+                    <div class="clearfix"></div>
+                @endif
             @endforeach
         </div>
 
