6ca348ec6fa574d6d8a3467a5abffe1a1f4f2500
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-23 23:10:22 +0700

    ss

--- resources/views/guest/chi-tiet-tin.blade.php
+++ resources/views/guest/chi-tiet-tin.blade.php
@@ -358,9 +358,9 @@
 					{!! $tin->noidung !!}
 				</div>
 
-				@if (!empty($lct->media_id))
-					<object data="{{ $lct->media->directory.'/'.$lct->media->filename.'.'.$lct->media->extension }}" type="application/pdf" width="100%" height="640px">
-						<p><b>Example fallback content</b>: This browser does not support PDFs. Please download the PDF to view it: <a href="{{ $lct->media->directory.'/'.$lct->media->filename.'.'.$lct->media->extension }}">Download PDF</a>.</p>
+				@if (!empty($tin->media_id))
+					<object data="{{ $tin->media->directory.'/'.$tin->media->filename.'.'.$tin->media->extension }}" type="application/pdf" width="100%" height="640px">
+						<p><b>Example fallback content</b>: This browser does not support PDFs. Please download the PDF to view it: <a href="{{ $tin->media->directory.'/'.$tin->media->filename.'.'.$tin->media->extension }}">Download PDF</a>.</p>
 					</object>
 
 				@endif
