d4d7a04aa1b11adb827b252e93404ed614bf9d9e
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-23 23:04:56 +0700

    ss

--- resources/views/guest/chi-tiet-tin.blade.php
+++ resources/views/guest/chi-tiet-tin.blade.php
@@ -267,7 +267,7 @@
 					</table>
 				</div>
 
-				<object data="{{ $tvb->path }}" type="application/pdf" width="100%" height="100%">
+				<object data="{{ $tvb->path }}" type="application/pdf" width="100%" height="640px">
 					<p><b>Example fallback content</b>: This browser does not support PDFs. Please download the PDF to view it: <a href="{{ $tvb->path }}">Download PDF</a>.</p>
 				</object>
 
