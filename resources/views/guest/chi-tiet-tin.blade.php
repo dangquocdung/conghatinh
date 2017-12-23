aa9c0a685aeaab4c1bc4d7f1f0cfb89c35c21626
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-23 23:03:39 +0700

    ss

--- resources/views/guest/chi-tiet-tin.blade.php
+++ resources/views/guest/chi-tiet-tin.blade.php
@@ -267,6 +267,12 @@
 					</table>
 				</div>
 
+				<object data="{{ $tvb->path }}" type="application/pdf" width="100%" height="100%">
+					<p><b>Example fallback content</b>: This browser does not support PDFs. Please download the PDF to view it: <a href="{{ $tvb->path }}">Download PDF</a>.</p>
+				</object>
+
+				<hr>
+
 				@include('guest.layout.tien-ich')
 
 
