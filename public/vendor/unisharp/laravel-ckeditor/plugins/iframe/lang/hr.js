9abaf3ff718c1db31a5fd1f4cd8a80632a493f87
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-24 13:14:31 +0700

    ss

--- public/vendor/unisharp/laravel-ckeditor/plugins/iframe/lang/hr.js
+++ public/vendor/unisharp/laravel-ckeditor/plugins/iframe/lang/hr.js
@@ -1,10 +1,10 @@
 /*
 Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'iframe', 'hr', {
 	border: 'Prikaži okvir IFrame-a',
-	noUrl: 'Unesite URL IFrame-a',
+	noUrl: 'Unesite URL iframe-a',
 	scrolling: 'Omogući trake za skrolanje',
 	title: 'IFrame svojstva',
 	toolbar: 'IFrame'
