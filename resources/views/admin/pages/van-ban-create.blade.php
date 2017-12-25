f08bea868a237077e9011d7e7c98e7fb97a1c49b
Quoc Dung Dang <dungthinhvn@gmail.com>
2017-12-25 07:46:50 +0700

    ss

--- resources/views/admin/pages/van-ban-create.blade.php
+++ resources/views/admin/pages/van-ban-create.blade.php
@@ -84,10 +84,11 @@
               <!-- Date -->
               <div class="form-group">
                   <div class='input-group date' id='datetimepicker_ngaybanhanh'>
-                      <input name="ngaybanhanh" type='text' class="form-control" value="{{ Carbon\Carbon::now()->format('d/m/Y') }}" required/>
                       <span class="input-group-addon">
-                            <span class="glyphicon glyphicon-calendar"></span>
-                        </span>
+                        <span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                      <input name="ngaybanhanh" type='text' class="form-control" value="{{ Carbon\Carbon::now()->format('d/m/Y') }}" required/>
+
                   </div>
                   <!-- /.input group -->
               </div>
