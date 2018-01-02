26db6a78a4a1fa32b3ea69968b2cf7048ded9f85
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-01-02 16:08:05 +0700

    ss:

--- resources/views/guest/layout/header-banner.blade.php
+++ resources/views/guest/layout/header-banner.blade.php
@@ -1,7 +1,7 @@
 <div class="container nen-trang">
     <div class="margin-15px">
 {{--        @include('guest.layout.header-slide')--}}
-        <div>
+        <div style="position: relative">
             <a href="/">
                 <img u="image" src="{{$toppic->hinhanh}}" width="100%"/>
             </a>
