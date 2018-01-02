e1095c2f6246a1d04eb77fa93501683f640e5076
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-01-02 15:54:54 +0700

    ss

--- resources/views/guest/layout/header-banner.blade.php
+++ resources/views/guest/layout/header-banner.blade.php
@@ -5,14 +5,15 @@
             <a href="/">
                 <img u="image" src="{{$toppic->hinhanh}}" width="100%"/>
             </a>
+            <div id="google_translate_element"></div>
+            <script type="text/javascript">
+                function googleTranslateElementInit() {
+                    new google.translate.TranslateElement({pageLanguage: 'vi', includedLanguages: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+                }
+            </script>
+            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
         </div>
-        <div id="google_translate_element"></div>
-        <script type="text/javascript">
-            function googleTranslateElementInit() {
-                new google.translate.TranslateElement({pageLanguage: 'vi', includedLanguages: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-            }
-        </script>
-        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
         @include('guest.layout.menu-ngang')
         @include('guest.layout.menu-ngang-mb')
