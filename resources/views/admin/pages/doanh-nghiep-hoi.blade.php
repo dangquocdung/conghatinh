36d7907ec940eaed8b36af02b3a09b6b48cc55c9
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-01-14 17:19:19 +0700

    ss

--- resources/views/admin/pages/doanh-nghiep-hoi.blade.php
+++ resources/views/admin/pages/doanh-nghiep-hoi.blade.php
@@ -20,98 +20,111 @@
           <h3 class="box-title">Doanh nghiệp hỏi - CQNN trả lời</h3>
         </div>
         <!-- /.box-header -->
-        <div class="box-body table-responsive">
-          <table class="table table-bordered table-striped table-hover">
-            <thead>
-            <tr>
-              <th>TT</th>
-              <th>Doanh nghiệp</th>
-              <th>Địa chỉ</th>
-              <th>Câu hỏi</th>
-              <th>Cơ quan trả lời </th>
-              <th>Câu Trả lời</th>
 
-            </tr>
-            </thead>
-            <tbody>
+          <div class="box-body table-responsive">
 
-            @foreach($dnhoi as $dnh)
-              <tr>
-                <td>
-                  @if ($dnh->daduyet == '1')
-                    {{--<span class="label label-success">Đã duyệt đăng</span>--}}
-                    <div class="pull-left gap-left gap-10">
-                      <confirm-modal
-                              btn-text='Hủy duyệt'
-                              btn-class="btn-danger"
-                              url="{{ route('duyet-doanh-nghiep-hoi') }}"
-                              :post-data="{{json_encode(['id' => $dnh->id, 'duyetdang'=>'0'])}}"
-                              :refresh="true"
-                              message="Bạn chắc chắn muốn thôi duyệt đăng câu hỏi?">
-                      </confirm-modal>
-                    </div>
+              <div class="table-responsive">
 
-                  @else
-                    <div class="pull-left gap-left gap-10">
-                      <confirm-modal
-                              btn-text='Chờ duyệt'
-                              btn-class="btn-success"
-                              url="{{ route('duyet-doanh-nghiep-hoi') }}"
-                              :post-data="{{json_encode(['id' => $dnh->id, 'duyetdang'=>'1'])}}"
-                              :refresh="true"
-                              message="Bạn chắc chắn muốn duyệt đăng câu hỏi?">
-                      </confirm-modal>
-                    </div>
-
-                    {{--<a href="#"><span class="label label-warning">Chờ duyệt...</span></a>--}}
-                  @endif
-
-                  <div class="pull-left gap-left gap-10">
-                    <confirm-modal
-                            btn-text='Xóa'
-                            btn-class="btn-danger"
-                            url="{{ route('delete-doanh-nghiep-hoi') }}"
-                            :post-data="{{json_encode(['id' => $dnh->id])}}"
-                            :refresh="true"
-                            message="Bạn chắc chắn muốn xóa?">
-                    </confirm-modal>
-                  </div>
-                </td>
-                <td>{{ \Carbon\Carbon::parse($dnh->created_at)->format('d-m-Y H:i:s') }} <br>{{ $dnh->doanhnghiep }}</td>
-                <td>{{ $dnh->diachi }}</td>
-                <td class="col-md-3 more">
-                  {!! $dnh->cauhoi !!}
-                </td>
-                <td>{{ $dnh->coquantraloi }}</td>
-                <td class="col-md-3">
-                  @if (strlen($dnh->cautraloi) >0 )
-
-                      <a class="btn btn-info btn-xs chinh-sua" data-toggle="modal" data-target="#tra-loi" dnh-id="{{ $dnh->id }}" cq-id="{{$dnh->coquan_id}}" ntl="{{$dnh->nguoitraloi}}" cv="{{$dnh->chucvu}}" ctl="{{$dnh->cautraloi}}">Chỉnh sửa</a>
+                  <table class="table table-bordered table-striped table-hover tblTinTuc">
+                      <thead>
+                      <tr>
+                          <th>TT</th>
+                          <th>Doanh nghiệp</th>
+                          <th>Địa chỉ</th>
+                          <th>Câu hỏi</th>
+                          <th>Cơ quan trả lời </th>
+                          <th>Câu Trả lời</th>
+                      </tr>
+                      </thead>
+                          <tbody>
 
 
+                          @foreach($dnhoi as $dnh)
+                              <tr>
+                                  <td>
+                                      {{ $dnh->id }}
+                                      @if ($dnh->daduyet == '1')
+                                          {{--<span class="label label-success">Đã duyệt đăng</span>--}}
+                                          <div class="pull-left gap-left gap-10">
+                                              <confirm-modal
+                                                      btn-text='Hủy duyệt'
+                                                      btn-class="btn-danger"
+                                                      url="{{ route('duyet-doanh-nghiep-hoi') }}"
+                                                      :post-data="{{json_encode(['id' => $dnh->id, 'duyetdang'=>'0'])}}"
+                                                      :refresh="true"
+                                                      message="Bạn chắc chắn muốn thôi duyệt đăng câu hỏi?">
+                                              </confirm-modal>
+                                          </div>
 
-                    Ngày {{ \Carbon\Carbon::parse($dnh->ngaytraloi)->format('d-m-Y H:i:s') }} <br>
-                    {{--<strong>{{ $dnh->nguoitraloi }}</strong> --}}
-                        {{--<em>({{ $dnh->chucvu }}):</em>--}}
-                    <span class="more">
+                                      @else
+                                          <div class="pull-left gap-left gap-10">
+                                              <confirm-modal
+                                                      btn-text='Chờ duyệt'
+                                                      btn-class="btn-success"
+                                                      url="{{ route('duyet-doanh-nghiep-hoi') }}"
+                                                      :post-data="{{json_encode(['id' => $dnh->id, 'duyetdang'=>'1'])}}"
+                                                      :refresh="true"
+                                                      message="Bạn chắc chắn muốn duyệt đăng câu hỏi?">
+                                              </confirm-modal>
+                                          </div>
+
+                                          {{--<a href="#"><span class="label label-warning">Chờ duyệt...</span></a>--}}
+                                      @endif
+
+                                      <div class="pull-left gap-left gap-10">
+                                          <confirm-modal
+                                                  btn-text='Xóa'
+                                                  btn-class="btn-danger"
+                                                  url="{{ route('delete-doanh-nghiep-hoi') }}"
+                                                  :post-data="{{json_encode(['id' => $dnh->id])}}"
+                                                  :refresh="true"
+                                                  message="Bạn chắc chắn muốn xóa?">
+                                          </confirm-modal>
+                                      </div>
+                                  </td>
+                                  <td>{{ \Carbon\Carbon::parse($dnh->created_at)->format('d-m-Y H:i:s') }} <br>{{ $dnh->doanhnghiep }}</td>
+                                  <td>{{ $dnh->diachi }}</td>
+                                  <td class="col-md-3 more">
+                                      {!! $dnh->cauhoi !!}
+                                  </td>
+                                  <td>{{ $dnh->coquantraloi }}</td>
+                                  <td class="col-md-3">
+                                      @if (strlen($dnh->cautraloi) >0 )
+
+                                          <a class="btn btn-info btn-xs chinh-sua" data-toggle="modal" data-target="#tra-loi" dnh-id="{{ $dnh->id }}" cq-id="{{$dnh->coquan_id}}" ntl="{{$dnh->nguoitraloi}}" cv="{{$dnh->chucvu}}" ctl="{{$dnh->cautraloi}}">Chỉnh sửa</a>
+
+
+
+                                          Ngày {{ \Carbon\Carbon::parse($dnh->ngaytraloi)->format('d-m-Y H:i:s') }} <br>
+                                          {{--<strong>{{ $dnh->nguoitraloi }}</strong> --}}
+                                          {{--<em>({{ $dnh->chucvu }}):</em>--}}
+                                          <span class="more">
                       {!! $dnh->cautraloi !!}
 
                     </span>
 
-                    @else
+                                      @else
 
-                    <div class="pull-left gap-left gap-10">
-                      <a class="btn btn-info btn-xs tra-loi" data-toggle="modal" data-target="#tra-loi" dnh-id="{{ $dnh->id }}" cq-id="{{$dnh->coquan_id}}">Trả lời</a>
-                    </div>
-                  @endif
+                                          <div class="pull-left gap-left gap-10">
+                                              <a class="btn btn-info btn-xs tra-loi" data-toggle="modal" data-target="#tra-loi" dnh-id="{{ $dnh->id }}" cq-id="{{$dnh->coquan_id}}">Trả lời</a>
+                                          </div>
+                                      @endif
 
-                </td>
+                                  </td>
 
-              </tr>
-            @endforeach
-            </tbody>
-          </table>
-        </div>
+                              </tr>
+
+                              @endforeach
+
+
+                          </tbody>
+                  </table>
+
+
+              </div>
+
+
+          </div>
         <!-- /.box-body -->
       </div>
       {{--End box--}}
