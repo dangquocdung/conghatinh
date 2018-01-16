f86abd49943156dd2aaa253728ae045270763e6e
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-01-16 13:29:02 +0700

    ss

---
+++ database/migrations/2018_01_16_132035_add_thongbao_to_lichcongtac_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddThongbaoToLichcongtacTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lich_cong_tacs', function (Blueprint $table) {
+            $table->boolean('thongbao')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lich_cong_tacs', function (Blueprint $table) {
+            $table->dropColumn('thongbao');
+        });
+    }
+}
