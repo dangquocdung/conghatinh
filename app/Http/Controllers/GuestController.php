fd7050eb274b0a75460e959bfb1347685b0172f2
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-01-16 15:21:59 +0700

    ss

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -51,7 +51,7 @@ class GuestController extends Controller
         $vbmoinhat = VanBan::where('daduyet','1')->orderby('id','desc')->take(5)->get();
         $lctmoinhat = LichCongTac::where('daduyet','1')->orderby('id','desc')->take(5)->get();
         $tinnoibat = TinTuc::where('daduyet','1')->where('noibat','1')->orderBy('id', 'decs')->take(8)->get();
-        $tinvan = TinTuc::where('tinvan','1')->orderBy('id', 'decs')->take(8)->pluck('slug','gioithieu')->toArray();
+        $tinvan = TinTuc::where('tinvan','1')->orderBy('id', 'decs')->take(8)->pluck('gioithieu')->toArray();
 
         $thongbao = LichCongTac::where('daduyet','1')->where('thongbao','1')->orderby('id','desc')->take(5)->get();
 
