9a50d0800a28683843cd8eb43fc7ebaed2831895
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-01-25 11:20:42 +0700

    ss

--- resources/views/guest/cong-bo-ttdn.blade.php
+++ resources/views/guest/cong-bo-ttdn.blade.php
@@ -35,7 +35,7 @@
                     <tr>
                         <td>{{ $loop->iteration }}</td>
                         <td>{{ $cb->name }}</td>
-                        <td>{{ $cb->thongtin }}</td>
+                        <td>{!! $cb->thongtin !!}</td>
                     </tr>
                 @endforeach
                 </tbody>
