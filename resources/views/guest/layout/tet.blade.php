91d198e91fc3a89aae5e3e57435918487af06772
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-02-02 10:38:43 +0700

    ss

--- resources/views/guest/layout/tet.blade.php
+++ resources/views/guest/layout/tet.blade.php
@@ -1,8 +1,8 @@
 <style type="text/css">body{padding-bottom:20px}</style>
 
-<img class="hidden-xs" style="position:fixed;z-index:9999;top:0;left:0;" src="http://4.bp.blogspot.com/-Vlt_TV7Q9V8/UsA7lwMZryI/AAAAAAAAA3Q/ref7HQc1968/s1600/banner_left.png" />
+<img class="hidden-xs" style="position:fixed;z-index:9999;top:0;left:0;" src="/uploads/hinh-anh/2018/2/banner_left_r3u.png" />
 
-<img class="hidden-xs" style="position:fixed;z-index:9999;top:0;right:0;" src="http://4.bp.blogspot.com/-A85wPjYE2BM/UsA7mDDQWmI/AAAAAAAAA3U/R9bxy6zSrLs/s1600/banner_right.png"/>
+<img class="hidden-xs" style="position:fixed;z-index:9999;top:0;right:0;" src="/uploads/hinh-anh/2018/2/banner_right_7xr.png"/>
 
 {{--<div style="position:fixed;z-index:9999;bottom:-50px;left:0;width:100%;height:104px;background:url(https://3.bp.blogspot.com/-uQrQaR3IkxE/WF9dDUUVLLI/AAAAAAAAAdw/VKNA5q7FJSQX5OWofOiPafEEENaoBcY9wCLcB/s1600/nentet.png) repeat-x bottom left;"></div>--}}
 
