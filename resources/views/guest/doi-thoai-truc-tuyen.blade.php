fb1cce0978a3a6ac2c8f6764b23e0d7a59071fc1
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-02-02 14:13:37 +0700

    ss

--- resources/views/guest/doi-thoai-truc-tuyen.blade.php
+++ resources/views/guest/doi-thoai-truc-tuyen.blade.php
@@ -4,6 +4,45 @@
 @endsection
 
 @section('content-main')
+
+    <div class="mainvideo">
+
+        <div class="doithoai">
+            <div class="videoIntro">
+
+                <p>
+                    <img src="skin-home/images/trung-tam-hanh-chinh.jpg" class="imgDoithoaiIntro">
+                </p>
+
+            </div>
+            <div class="infodoithoaiIntro">
+                <p class="titleIntro">KÊNH ĐỐI THOẠI TRỰC TUYẾN</p>
+                <p>
+                    <br>
+                </p>
+                <p class="contentIntro">
+                    Nhằm tăng cường hơn nữa việc tiếp nhận, trao đổi thông tin, Cổng thông tin điện tử tỉnh xây dựng và mở chuyên mục đối thoại trực tuyến. Mong rằng đây sẽ là một kênh thông tin hữu ích giúp các cơ quan nhà nước đến gần hơn với nhân dân, cộng đồng doanh nghiệp.
+                </p>
+                <p>
+                </p><p class="contentIntro">
+                    Quý vị quan tâm đến Đối thoại trực tuyến có thể tham gia và gửi câu hỏi trực tiếp bằng cách truy cập địa chỉ <a class="introlink" href="http://www.doithoai.danang.gov.vn">www.doithoai.danang.gov.vn</a>, gửi thư điện tử về <a class="introlink" href="mailto:doithoai@danang.gov.vn">doithoai@danang.gov.vn</a> hoặc trực tiếp gọi số 0236.1022.
+                </p>
+                <p class="contentIntro">
+
+                    <br>
+
+                </p><p class="bold">Trân trọng. </p>
+
+
+                <p class="contentIntroRight">BAN BIÊN TẬP CỔNG TTĐT TỈNH HÀ TĨNH </p>
+                <p></p>
+
+            </div>
+        </div>
+
+    </div>
+
+
     <div class="block3">
 
         <div class="portlet-header">
