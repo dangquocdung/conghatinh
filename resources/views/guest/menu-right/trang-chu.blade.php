d0d4c5451b9dd545dff2feb535649905ee31f035
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-02-02 16:56:32 +0700

    ss

--- resources/views/guest/menu-right/trang-chu.blade.php
+++ resources/views/guest/menu-right/trang-chu.blade.php
@@ -47,7 +47,17 @@
 
     <div class="block4">
         <script type="text/javascript">
-            showWeather('hanoi, vietnam');
+            Weather.getCurrent("Kansas City", function(current) {
+                console.log(
+                    ["currently:",current.temperature(),"and",current.conditions()].join(" ")
+                );
+            });
+
+            Weather.getForecast("Kansas City", function(forecast) {
+                console.log("Forecast High in Kelvin: " + forecast.high());
+                console.log("Forecast High in Fahrenheit" + Weather.kelvinToFahrenheit(forecast.high()));
+                console.log("Forecast High in Celsius" + Weather.kelvinToCelsius(forecast.high()));
+            });
         </script>
     </div>
 
