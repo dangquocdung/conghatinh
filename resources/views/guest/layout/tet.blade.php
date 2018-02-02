3f95b1fa4fe0788269c12a376588d269668d1a84
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-02-02 10:29:42 +0700

    ss

--- resources/views/guest/layout/tet.blade.php
+++ resources/views/guest/layout/tet.blade.php
@@ -4,6 +4,6 @@
 
 <img class="hidden-xs" style="position:fixed;z-index:9999;top:0;right:0;" src="http://4.bp.blogspot.com/-A85wPjYE2BM/UsA7mDDQWmI/AAAAAAAAA3U/R9bxy6zSrLs/s1600/banner_right.png"/>
 
-<div style="position:fixed;z-index:9999;bottom:-50px;left:0;width:100%;height:104px;background:url(https://3.bp.blogspot.com/-uQrQaR3IkxE/WF9dDUUVLLI/AAAAAAAAAdw/VKNA5q7FJSQX5OWofOiPafEEENaoBcY9wCLcB/s1600/nentet.png) repeat-x bottom left;"></div>
+{{--<div style="position:fixed;z-index:9999;bottom:-50px;left:0;width:100%;height:104px;background:url(https://3.bp.blogspot.com/-uQrQaR3IkxE/WF9dDUUVLLI/AAAAAAAAAdw/VKNA5q7FJSQX5OWofOiPafEEENaoBcY9wCLcB/s1600/nentet.png) repeat-x bottom left;"></div>--}}
 
 <img style="position:fixed;z-index:9999;bottom:20px;left:20px" src="uploads/hinh-anh/2018/2/bottom_tet_8sk.png"/>
